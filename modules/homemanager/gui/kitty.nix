d25300a2e03ceacfc25264a0e5014bf25ac40f0e
Alex-Tudor Petrean <alex@padfoot.cc>
2024-12-28 01:35:35 +0200

    friendship ended with other terminals

--- modules/homemanager/gui/kitty.nix
+++
@@ -1,6 +0,0 @@
-{ lib, ... }: {
-  programs.kitty = {
-    enable = true;
-    extraConfig = lib.readFile ../../../dotfiles/kitty/kitty.conf;
-  };
-}
--- modules/homemanager/gui/wezterm.nix
+++
@@ -1,6 +0,0 @@
-{ lib, ... }: {
-  programs.wezterm = {
-    enable = true;
-    extraConfig = lib.readFile ../../../dotfiles/wezterm/wezterm.lua;
-  };
-}
