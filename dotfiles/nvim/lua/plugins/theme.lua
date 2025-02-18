9c1d988c5146d610450461db723dc0c9892461e8
Alex Petrean <alex-tudor.petrean@snyk.io>
2025-02-18 19:37:37 +0200

    catppuccin

--- dotfiles/nvim/lua/plugins/theme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Mofiqul/vscode.nvim",
-  config = function()
-    local colours = require("vscode.colors").get_colors
-    require("vscode").setup({
-      italic_comments = true,
-    })
-  end,
-}
