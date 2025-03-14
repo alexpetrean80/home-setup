8794fe5fd29f52213fe89593c1afe46ffb21a917
Alex Petrean <alex-tudor.petrean@snyk.io>
2025-03-14 11:22:34 +0200

    add render-markdown.nvim

---
+++ dotfiles/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,11 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  opts = {
+    completions = {
+      lsp = {
+        enabled = true,
+      },
+    },
+  },
+}
