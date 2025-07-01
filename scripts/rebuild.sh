28e4ee3d92421df7d81cbcb1f78a7dc07ccf0121
Alex-Tudor Petrean <alex@padfoot.cc>
2025-07-01 16:41:37 +0300

    fix rebuild script

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -5,25 +5,37 @@ git add .
 # Always be in the repo.
 cd "$HOME/Repos/home-setup" || exit 1
 
-# NOTE: Naive way of making the script only do the bare minimum.
-# By convention the files are named in such a way such that
-# I can see if a part of the flake has changed and
-# only rebuild the required parts.
-# This method rebuilds for refactors/reformats, but it's
-# better than nothing.
-nixos_changed=$(git status | rg "nixos" | wc -l)
-nix_darwin_changed=$(git status | rg "darwin" | wc -l)
-hm_chaned=$(git status | rg "homemanager|scripts" | wc -l) # also check if scripts changed as they are installed via home-manager
+while getopts 'n' opt; do
+	case "$opt" in
+	n)
+		# flag to always uppdate. useful when the config changes on another machine and another must be brought up to date
+		nixos_changed=1
+		nix_darwin_changed=1
+		hm_changed=1
+		;;
+	?)
+		# NOTE: Naive way of making the script only do the bare minimum.
+		# By convention the files are named in such a way such that
+		# I can see if a part of the flake has changed and
+		# only rebuild the required parts.
+		# This method rebuilds for refactors/reformats, but it's
+		# better than nothing.
+		nixos_changed=$(git status | rg "nixos" | wc -l)
+		nix_darwin_changed=$(git status | rg "darwin" | wc -l)
+		hm_changed=$(git status | rg "homemanager|scripts" | wc -l) # also check if scripts changed as they are installed via home-manager
 
-if [[ $nixos_changed -eq 0 && $hm_chaned -eq 0 && $nix_darwin_changed -eq 0 ]]; then
-	gum log --level='info' 'no op. exiting...'
-	exit 0
-fi
+		if [[ $nixos_changed -eq 0 && $hm_changed -eq 0 && $nix_darwin_changed -eq 0 ]]; then
+			gum log --level='info' 'no op. exiting...'
+			exit 0
+		fi
+		;;
+	esac
+done
 
 case "$(uname)" in
 
 "Linux")
-	if [[ nixos_changed -ne 0 ]]; then
+	if [[ $nixos_changed -ne 0 ]]; then
 		echo "rebuilding $(gum style --italic --foreground 99 'Linux') machine..."
 		sudo nixos-rebuild switch --flake . --impure
 		echo "$(gum style --italic --foreground 99 'NixOS') rebuilt successfully."
@@ -31,11 +43,11 @@ case "$(uname)" in
 
 	#shellcheck disable=SC2043
 	for flatpak in "app.zen_browser.zen"; do
-		flatpak install flathub "$flatpak"
+		flatpak update "$flatpak"
 	done
 	;;
 "Darwin")
-	if [[ nix_darwin_changed -ne 0 ]]; then
+	if [[ $nix_darwin_changed -ne 0 ]]; then
 		echo "rebuilding $(gum style --italic --foreground 99 'MacOS') machine..."
 		darwin-rebuild switch --flake .
 		echo "$(gum style --italic --foreground 99 'nix-darwin') rebuilt successfully."
@@ -46,7 +58,7 @@ case "$(uname)" in
 	;;
 esac
 
-if [[ hm_chaned -ne 0 ]]; then
+if [[ $hm_changed -ne 0 ]]; then
 	echo "rebuilding $(gum style --italic --foreground 99 'home-manager')..."
 	home-manager switch --flake . --impure
 	echo "$(gum style --italic --foreground 99 'home-manager') rebuilt successfully."
