6a7fcaef7081bc1b03240e1337acf5b5f529278d
Alex Petrean <alex-tudor.petrean@snyk.io>
2025-07-31 17:16:07 +0300

    neovim nightly + builtin package management

--- modules/homemanager/cli/neovim.nix
+++ modules/homemanager/cli/neovim.nix
@@ -1,10 +1,12 @@
 {
+inputs,
   config,
   pkgs,
   ...
 }: {
   programs.neovim = {
     enable = true;
+package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
   };
 
   home.file.".config/nvim" = {
