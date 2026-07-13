95b1fc5343935d458ee830256841990cf8ee24e7
Alex Petrean <alex.petrean@mailbox.org>
2026-07-13 11:53:44 +0300

    chore: sync dotfiles from chezmoi
    Refresh the embedded dotfile mirror from the local chezmoi source (the machine's source of truth): JetBrainsMono + light/dark theme switching for ghostty (with themes/), starship catppuccin palettes, zed themes, and the newer nvim init.lua/pack-lock.

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -71,8 +71,6 @@ vim.pack.add({
     { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
     { src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
     { src = "https://github.com/qvalentin/helm-ls.nvim" },
-    { src = "https://github.com/olimorris/codecompanion.nvim" },
-    { src = "https://github.com/milanglacier/minuet-ai.nvim" },
 })
 
 -- [[ SECTION: Plugin setup ]]
@@ -92,11 +90,11 @@ require("conform").setup({
         markdown = { "prettier", "prettierd" },
         graphql = { "prettier", "prettierd" },
         python = { "isort", "black" },
+        ruby = { "rubocop" },
         terraform = { "terraform_fmt" },
         tf = { "terraform_fmt" },
         ["terraform-vars"] = { "terraform_fmt" },
         hcl = { "terraform_fmt" },
-        nix = { "alejandra" },
     },
 })
 
@@ -106,6 +104,7 @@ require("lint").linters_by_ft = {
     javascriptreact = { "eslint" },
     typescript = { "eslint" },
     typescriptreact = { "eslint" },
+    ruby = { "rubocop" },
 }
 
 require("mini.starter").setup()
@@ -159,35 +158,6 @@ require("luasnip").config.set_config({
     enable_autosnippets = true,
 })
 require("luasnip.loaders.from_vscode").lazy_load()
-
-require("minuet").setup({
-    provider = "openai_fim_compatible",
-    n_completions = 1,
-    context_window = 512,
-    virtualtext = {
-        auto_trigger_ft = { "*" },
-        keymap = {
-            accept = "<Tab>",
-            accept_line = "<A-a>",
-            accept_n_lines = "<A-z>",
-            prev = "<A-[>",
-            next = "<A-]>",
-            dismiss = "<A-e>",
-        },
-    },
-    provider_options = {
-        openai_fim_compatible = {
-            api_key = "TERM",
-            name = "Ollama",
-            end_point = "http://localhost:11434/v1/completions",
-            model = "qwen2.5-coder:1.5b",
-            optional = {
-                max_tokens = 56,
-                top_p = 0.9,
-            },
-        },
-    },
-})
 
 require("blink.cmp").setup({
     keymap = {
@@ -247,21 +217,17 @@ require("mason-lspconfig").setup({
         "jsonls",
         "lua_ls",
         "marksman",
-        "nixd",
         "pyright",
         "ruff",
-        "spectral",
-        "sqls",
+        "ruby_lsp",
         "terraformls",
         "tflint",
-        "ts_ls",
         "vtsls",
         "yamlls",
     },
 })
 require("mason-tool-installer").setup({
     ensure_installed = {
-        "alejandra",
         "black",
         "debugpy",
         "eslint_d",
@@ -271,6 +237,7 @@ require("mason-tool-installer").setup({
         "isort",
         "prettier",
         "prettierd",
+        "rubocop",
         "stylua",
     },
 })
@@ -333,47 +300,6 @@ require("neotest").setup({
         require("neotest-golang")(),
         require("neotest-python")({ dap = { justMyCode = false } }),
         require("neotest-jest")({}),
-    },
-})
-
--- CodeCompanion with Cursor ACP
--- Prerequisites: cursor-agent login, then npm install -g @blowmage/cursor-agent-acp
--- Wait for "Cursor" to appear in the chat buffer header before sending messages
-require("codecompanion").setup({
-    interactions = {
-        chat = {
-            adapter = "cursor_acp",
-        },
-    },
-    display = {
-        action_palette = {
-            provider = "snacks",
-        },
-        chat = {
-            window = {
-                position = "right",
-            },
-        },
-    },
-    adapters = {
-        acp = {
-            cursor_acp = function()
-                return require("codecompanion.adapters").extend("cagent", {
-                    name = "cursor_acp",
-                    formatted_name = "Cursor",
-                    commands = {
-                        -- Use npx so the adapter is found even if not globally installed
-                        default = { "npx", "--yes", "@blowmage/cursor-agent-acp" },
-                    },
-                    defaults = {
-                        timeout = 60000, -- 60s for slower startup
-                    },
-                })
-            end,
-        },
-    },
-    opts = {
-        log_level = "DEBUG", -- Set to "INFO" once working; run :checkhealth codecompanion for log path
     },
 })
 
@@ -448,6 +374,21 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    group = augroup,
+    callback = function(args)
+        local buf = args.buf
+        vim.keymap.set("n", "<leader>lf", "<cmd>GoFmt<cr>", { desc = "Format", buffer = buf, silent = true })
+        vim.keymap.set("n", "gc", "<cmd>GoCmt<cr>", { desc = "Comment", buffer = buf, silent = true })
+        vim.keymap.set("n", "<leader>lI", "<cmd>GoImports<cr>", { desc = "Imports", buffer = buf, silent = true })
+        vim.keymap.set("n", "<leader>lA", "<cmd>GoAddTag<cr>", { desc = "Add Tags", buffer = buf, silent = true })
+        vim.keymap.set("n", "<leader>lR", "<cmd>GoRmTag<cr>", { desc = "Remove Tags", buffer = buf, silent = true })
+        vim.keymap.set("n", "<leader>ls", "<cmd>GoFillStruct<cr>", { desc = "Fill Struct", buffer = buf, silent = true })
+        vim.keymap.set("n", "<leader>le", "<cmd>GoIfErr<cr>", { desc = "If Err", buffer = buf, silent = true })
+    end,
+})
+
 vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
     group = augroup,
     callback = function()
@@ -460,7 +401,7 @@ vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
 vim.lsp.config("*", {
     capabilities = require("blink.cmp").get_lsp_capabilities(),
 })
-vim.lsp.enable({ "lua_ls", "gopls", "ts_ls", "sqls", "terraformls", "pyright", "eslint", "helm_ls", "yamlls", "tflint", "ruff", "jsonls", "nixd" })
+vim.lsp.enable({ "lua_ls", "gopls", "ts_ls", "sqls", "terraformls", "pyright", "eslint", "helm_ls", "yamlls", "tflint", "ruff", "jsonls", "ruby_lsp" })
 
 vim.lsp.config("lua_ls", {
     settings = {
@@ -479,16 +420,6 @@ vim.lsp.config("gopls", {
                 compositeLiteralFields = true,
                 compositeLiteralTypes = true,
                 functionTypeParameters = true,
-            },
-        },
-    },
-})
-
-vim.lsp.config("nixd", {
-    settings = {
-        nixd = {
-            formatting = {
-                command = { "alejandra" },
             },
         },
     },
@@ -542,6 +473,8 @@ local function keymap(mode, lhs, rhs, desc)
 end
 
 keymap("n", "<leader>\\", "<cmd>vsplit<CR>", "Vertical split")
+keymap("v", "<", "<gv", "Indent left, keep selection")
+keymap("v", ">", ">gv", "Indent right, keep selection")
 Snacks.keymap.set("n", "<leader><leader>", function()
     Snacks.picker.files()
 end, { desc = "Files" })
@@ -681,13 +614,5 @@ keymap("n", "<leader>aa", "<cmd>CodeCompanionActions<cr>", "Action Palette")
 keymap("n", "<leader>ac", "<cmd>CodeCompanionChat Toggle<cr>", "Chat Toggle")
 keymap("v", "<leader>ac", "<cmd>CodeCompanionChat Add<cr>", "Chat Add (selection)")
 
--- Go keymaps via Snacks.keymap (filetype-specific)
-Snacks.keymap.set("n", "<leader>lf", "<cmd>GoFmt<cr>", { desc = "Go Format", ft = "go" })
-Snacks.keymap.set("n", "<leader>lI", "<cmd>GoImports<cr>", { desc = "Go Imports", ft = "go" })
-Snacks.keymap.set("n", "<leader>lA", "<cmd>GoAddTag<cr>", { desc = "Go Add Tags", ft = "go" })
-Snacks.keymap.set("n", "<leader>lR", "<cmd>GoRmTag<cr>", { desc = "Go Remove Tags", ft = "go" })
-Snacks.keymap.set("n", "<leader>ls", "<cmd>GoFillStruct<cr>", { desc = "Fill Struct", ft = "go" })
-Snacks.keymap.set("n", "<leader>le", "<cmd>GoIfErr<cr>", { desc = "If Err", ft = "go" })
-Snacks.keymap.set("n", "<leader>lc", "<cmd>GoCmt<cr>", { desc = "Comment", ft = "go" })
-
 -- [[ SECTION: Finish ]]
+vim.cmd.colorscheme("catppuccin")
